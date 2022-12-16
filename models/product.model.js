af362a6113eaba94bb4a2ea2a397546f6dfe3521
akashbhoi2815 <manishbhoi1997@gmail.com>
2022-12-16 11:30:05 +0530

    timestamp

--- models/product.model.js
+++ models/product.model.js
@@ -4,8 +4,10 @@ const userSchema = mongoose.Schema({
     title:{type:String,require:true},
     quantity:{type:String,require:true},
     priority:{type:String,require:true},
-    timestamp:{type:String,require:true},
     description:{type:String,require:true}
+}
+,{
+    timestamps: true 
 })
 
 const ProductModel = mongoose.model("user",userSchema);
